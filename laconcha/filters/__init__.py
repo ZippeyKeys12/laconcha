9248376caeccda0cabb687cc115832abfec3a52e
Zain Aamer <ZippeyKeys12@gmail.com>
2020-06-26 18:40:27 -0500

    Filter decorators

---
+++ laconcha/filters/__init__.py
@@ -0,0 +1,4 @@
+# pylint: disable-all
+# flake8: noqa
+
+from .decorators import filter_opencv, filter_pil, filter_scikit
---
+++ laconcha/filters/decorators.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+from typing import Callable
+
+import numpy as np
+from PIL.Image import Image as PILImage
+
+from ..image import Filter, Image
+
+OpenCVFilter = Callable[[np.ndarray], np.ndarray]
+
+
+def filter_opencv(f: OpenCVFilter) -> Filter:
+    def new_f(img: Image):
+        return Image.from_opencv(f(img.as_opencv()))
+
+    return new_f
+
+
+PILFilter = Callable[[PILImage], PILImage]
+
+
+def filter_pil(f: PILFilter) -> Filter:
+    def new_f(img: Image):
+        return Image.from_pil(f(img.as_pil()))
+
+    return new_f
+
+
+ScikitFilter = Callable[[np.ndarray], np.ndarray]
+
+
+def filter_scikit(f: ScikitFilter) -> Filter:
+    def new_f(img: Image):
+        return Image.from_scikit(f(img.as_scikit()))
+
+    return new_f
