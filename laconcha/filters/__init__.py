fb79f84cb56099fa53d7fc3b4ea29743b94f0464
Zain Aamer <ZippeyKeys12@gmail.com>
2020-06-29 11:09:58 -0500

    Swirl

--- laconcha/filters/__init__.py
+++ laconcha/filters/__init__.py
@@ -6,3 +6,4 @@ from .opencv import (ColorChannel, ColorMode, bilateral_filter, channel,
                      color_quantization, convert_color, gaussian_blur,
                      mean_filter, median_filter)
 from .pil import spread
+from .scikit import swirl
---
+++ laconcha/filters/scikit.py
@@ -0,0 +1,14 @@
+import numpy as np
+from skimage.transform import integral_image, probabilistic_hough_line, rotate
+from skimage.transform import swirl as sk_swirl
+
+from ..image import Filter
+from .decorators import filter_scikit
+
+
+def swirl(strength: float = 1, radius: float = 100, rotation: float = 0) -> Filter:
+    @filter_scikit
+    def f(img: np.ndarray) -> np.ndarray:
+        return sk_swirl(img, strength=strength, radius=radius, rotation=rotation)
+
+    return f
