9b96639ea014c95f83acbda1e38cf7cf5ab2e5de
Zain Aamer <ZippeyKeys12@gmail.com>
2020-06-30 22:38:07 -0500

    Update filter __init__

--- laconcha/filters/__init__.py
+++ laconcha/filters/__init__.py
@@ -1,10 +1,12 @@
 # pylint: disable-all
 # flake8: noqa
 
+from .base import identity
 from .decorators import filter_opencv, filter_pil, filter_scikit
 from .numpy import (ColorMode, bilateral_filter, color_quantization,
                     convert_color, gaussian_blur, integral, mean_filter,
                     median_filter, swirl)
 from .opencv import ColorChannel, channel
-from .pil import max_filter, min_filter, mode_filter, spread, unsharpen
+from .pil import (brightness, contrast, max_filter, min_filter, mode_filter,
+                  saturation, sharpness, spread, unsharpen)
 from .transforms import rotate, scale, shear, transform, translate
