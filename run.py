4f316b43e7f84bd0c377ffa1fe934178b6fe9850
Zain Aamer <ZippeyKeys12@gmail.com>
2020-07-03 16:31:32 -0500

    Update client for new filters
    Also use size to adjust filter parameters

--- run.py
+++ run.py
@@ -1,19 +1,26 @@
 # pylint: disable-all
+
 from typing import Any
 
 import streamlit as st
 
 import SessionState
 from laconcha import Image
+from laconcha.curves import (circular, cosine, cubic, exponential, inverse,
+                             logarithm, quadratic, sine, smooth_step,
+                             smoother_step, smoothest_step, square_root)
 from laconcha.filters import (ColorChannel, ColorMode, autocontrast,
-                              bilateral_filter, brightness, channel,
+                              bilateral_filter, bottom, brightness, channel,
                               color_quantization, contrast, convert_color,
-                              equalize, gaussian_blur, hflip, integral, invert,
-                              max_filter, mean_filter, median_filter,
-                              min_filter, mode_filter, posterize, rotate,
-                              saturation, scale, sharpness, shear, solarize,
-                              spread, swirl, translate, unsharpen, vflip)
+                              crop, curve, equalize, fit, gaussian_blur, hflip,
+                              hmirror, integral, invert, left, max_filter,
+                              mean_filter, median_filter, min_filter,
+                              mode_filter, oct_mirror, posterize, quad_mirror,
+                              right, rotate, saturation, scale, sharpness,
+                              shear, solarize, spread, swirl, top, translate,
+                              unsharpen, vflip, vmirror)
 from laconcha.generators import gaussian_noise, white_noise
+from laconcha.util import format_name
 
 # SETUP
 
@@ -32,102 +39,134 @@ generator_dict = {
 }
 
 filter_dict = {
-    'As OpenCV': (lambda: lambda i: Image.from_opencv(i.as_opencv()), lambda i: []),
-    'As PIL': (lambda: lambda i: Image.from_pil(i.as_pil()), lambda i: []),
-    'As Scikit': (lambda: lambda i: Image.from_scikit(i.as_scikit()), lambda i: []),
-    'Autocontrast': (autocontrast, lambda i: [
+    'As OpenCV': (lambda: lambda i: Image.from_opencv(i.as_opencv()), lambda i, h, w: []),
+    'As PIL': (lambda: lambda i: Image.from_pil(i.as_pil()), lambda i, h, w: []),
+    'As Scikit': (lambda: lambda i: Image.from_scikit(i.as_scikit()), lambda i, h, w: []),
+    'Autocontrast': (autocontrast, lambda i, h, w: [
         st.slider('Cutoff', 0, 49, 0, key=f'C{i}')
     ]),
-    'Bilateral Filter': (bilateral_filter, lambda i: [
+    'Bilateral Filter': (bilateral_filter, lambda i, h, w: [
         st.slider('Diameter', 1, 15, 9, key=f'D{i}'),
         st.slider('Std. Deviation (Color)', 0, 100, 75, key=f'C{i}'),
         st.slider('Std. Deviation (Space)', 0, 100, 75, key=f'S{i}')
     ]),
-    'Brightness': (brightness, lambda i: [
+    'Bottom': (bottom, lambda i, h, w: [
+        st.slider('Pixels', 0, h, h, key=f'P{i}')
+    ]),
+    'Brightness': (brightness, lambda i, h, w: [
         st.slider('Factor', 0.0, 2.0, 1.0, key=f'F{i}')
     ]),
-    'Channel': (channel, lambda i: [
+    'Channel': (channel, lambda i, h, w: [
         st.selectbox('Channel', list(ColorChannel),
                      format_func=lambda x: x.name, key=f'C{i}')
     ]),
-    'Color Quantization': (color_quantization, lambda i: [
+    'Color Quantization': (color_quantization, lambda i, h, w: [
         st.slider('# of Clusters', 1, 128, 16, key=f'#{i}')
     ]),
-    'Contrast': (contrast, lambda i: [
+    'Contrast': (contrast, lambda i, h, w: [
         st.slider('Factor', 0.0, 2.0, 1.0, key=f'F{i}')
     ]),
-    'Convert Color Mode': (convert_color, lambda i: [
+    'Convert Color Mode': (convert_color, lambda i, h, w: [
         st.selectbox('From', list(ColorMode),
                      format_func=lambda x: x.name, key=f'F{i}'),
         st.selectbox('To', list(ColorMode),
                      format_func=lambda x: x.name, key=f'T{i}')
     ]),
-    'Equalize': (equalize, lambda i: []),
-    'Gaussian Blur': (gaussian_blur, lambda i: [
+    'Crop': (crop, lambda i, h, w: [
+        st.slider('Height', 0, h, h, key=f'H{i}'),
+        st.slider('Width', 0, w, w, key=f'W{i}')
+    ]),
+    'Curve': (curve, lambda i, h, w: [
+        st.selectbox('Function', [
+            circular, cosine, cubic, exponential, inverse, logarithm,
+            sine, smooth_step, smoother_step, smoothest_step, square_root,
+            quadratic],
+            format_func=lambda x: format_name(x.__name__))
+    ]),
+    'Equalize': (equalize, lambda i, h, w: []),
+    'Fit': (fit, lambda i, h, w: [
+        st.slider('Height', 0, h, h, key=f'H{i}'),
+        st.slider('Width', 0, w, w, key=f'W{i}')
+    ]),
+    'Gaussian Blur': (gaussian_blur, lambda i, h, w: [
         (st.slider('Kernel Width', 1, 7, 5, 2, key=f'W{i}'),
          st.slider('Kernel Height', 1, 7, 5, 2, key=f'H{i}')),
         st.slider('Standard Deviation', 0.0, 10.0, 0.0, key=f'S{i}')
     ]),
-    'Horizontal Flip': (hflip, lambda i: []),
-    'Integral': (integral, lambda i: []),
-    'Invert': (invert, lambda i: []),
-    'Max Filter': (max_filter, lambda i: [
+    'Horizontal Flip': (hflip, lambda i, h, w: []),
+    'Horizontal Mirror': (hmirror, lambda i, h, w: []),
+    'Integral': (integral, lambda i, h, w: []),
+    'Invert': (invert, lambda i, h, w: []),
+    'Left': (left, lambda i, h, w: [
+        st.slider('Pixels', 0, w, w, key=f'P{i}')
+    ]),
+    'Max Filter': (max_filter, lambda i, h, w: [
         st.slider('Diameter', 1, 15, 9, 2, key=f'D{i}')
     ]),
-    'Mean Filter': (mean_filter, lambda i: [
+    'Mean Filter': (mean_filter, lambda i, h, w: [
         (st.slider('Kernel Width', 1, 7, 5, key=f'W{i}'),
          st.slider('Kernel Height', 1, 7, 5, key=f'H{i}'))
     ]),
-    'Median Filter': (median_filter, lambda i: [
+    'Median Filter': (median_filter, lambda i, h, w: [
         st.slider('Diameter', 1, 15, 9, 2, key=f'D{i}')
     ]),
-    'Min Filter': (min_filter, lambda i: [
+    'Min Filter': (min_filter, lambda i, h, w: [
         st.slider('Diameter', 1, 15, 9, 2, key=f'D{i}')
     ]),
-    'Mode Filter': (mode_filter, lambda i: [
+    'Mode Filter': (mode_filter, lambda i, h, w: [
         st.slider('Diameter', 1, 15, 9, 2, key=f'D{i}')
     ]),
-    'Posterize': (posterize, lambda i: [
+    'Oct-Mirror': (oct_mirror, lambda i, h, w: []),
+    'Posterize': (posterize, lambda i, h, w: [
         st.slider('Bits', 1, 8, 8, key=f'B{i}')
     ]),
-    'Rotate': (rotate, lambda i: [
+    'Quad-Mirror': (quad_mirror, lambda i, h, w: []),
+    'Right': (right, lambda i, h, w: [
+        st.slider('Pixels', 0, w, w, key=f'P{i}')
+    ]),
+    'Rotate': (rotate, lambda i, h, w: [
         st.slider('Angle', 0.0, 360.0, 0.0, key=f'A{i}')
     ]),
-    'Saturation': (saturation, lambda i: [
+    'Saturation': (saturation, lambda i, h, w: [
         st.slider('Factor', 0.0, 2.0, 1.0, key=f'F{i}')
     ]),
-    'Scale': (scale, lambda i: [
+    'Scale': (scale, lambda i, h, w: [
         (st.slider('Scale X', .1, 10.0, 1.0, key=f'X{i}'),
          st.slider('Scale Y', .1, 10.0, 1.0, key=f'Y{i}'))
     ]),
-    'Sharpness': (sharpness, lambda i: [
+    'Sharpness': (sharpness, lambda i, h, w: [
         st.slider('Factor', 0.0, 2.0, 1.0, key=f'F{i}')
     ]),
-    'Shear': (shear, lambda i: [
+    'Shear': (shear, lambda i, h, w: [
         st.slider('Angle', 0.0, 90.0, 0.0, key=f'A{i}')
     ]),
-    'Solarize': (solarize, lambda i: [
+    'Sine': (sine, lambda i, h, w: []),
+    'Solarize': (solarize, lambda i, h, w: [
         st.slider('Threshold', 0, 255, 128, key=f'B{i}')
     ]),
-    'Spread': (spread, lambda i: [
+    'Spread': (spread, lambda i, h, w: [
         st.slider('Distance', 0, 15, 3, key=f'D{i}')
     ]),
-    'Swirl': (swirl, lambda i: [
-        (st.number_input('Center X', key=f'X{i}'),
-         st.number_input('Center Y', key=f'Y{i}')),
+    'Swirl': (swirl, lambda i, h, w: [
+        (st.slider('Center X', 0, w, w // 2, key=f'X{i}'),
+         st.slider('Center Y', 0, h, h // 2, key=f'Y{i}')),
         st.slider('Strength', -100.0, 100.0, 0.0, key=f'S{i}'),
-        st.slider('Radius', 1.0, 2000.0, 100.0, key=f'R{i}')
+        st.slider('Radius', 1.0, float(max(h, w)), 100.0, key=f'R{i}')
     ]),
-    'Translate': (translate, lambda i: [
-        (st.slider('Offset X', -400, 400, 0, key=f'X{i}'),
-         st.slider('Offset Y', -400, 400, 0, key=f'Y{i}'))
+    'Top': (top, lambda i, h, w: [
+        st.slider('Pixels', 0, h, h, key=f'P{i}')
     ]),
-    'Unsharpen': (unsharpen, lambda i: [
+    'Translate': (translate, lambda i, h, w: [
+        (st.slider('Offset X', -w, w, 0, key=f'X{i}'),
+         st.slider('Offset Y', -h, h, 0, key=f'Y{i}'))
+    ]),
+    'Unsharpen': (unsharpen, lambda i, h, w: [
         st.slider('Radius', 0, 7, 2, key=f'R{i}'),
         st.slider('Percent', 0, 200, 150, key=f'P{i}'),
         st.slider('Threshold', 0, 10, 3, key=f'T{i}')
     ]),
-    'Vertical Flip': (vflip, lambda i: [])
+    'Vertical Flip': (vflip, lambda i, h, w: []),
+    'Vertical Mirror': (vmirror, lambda i, h, w: [])
 }
 
 # PAGE
@@ -151,11 +190,11 @@ st.image(img.as_pil(), use_column_width=True)
 for i in session_state.filters:
     st.markdown('---')
 
-    f = st.selectbox('Type', list(
-        filter_dict.keys()), key=f'FilterType{i}')
-    f, args = filter_dict[f]
+    f, f_args = filter_dict[st.selectbox('Type', list(
+        filter_dict.keys()), key=f'FilterType{i}')]
     with st.spinner('Applying Filter...'):
-        img = img.apply_filter(f(*args(i)))
+        img = img.apply_filter(f(*f_args(i, h, w)))  # type: ignore
+        h, w = img.size
 
     st.image(img.as_pil(), use_column_width=True)
 
