bad2b8fc908e273aef82bd2ca9eefba5b5975fe4
Zain Aamer <ZippeyKeys12@gmail.com>
2020-07-23 21:53:46 -0500

    Update util.py

--- laconcha/util.py
+++ laconcha/util.py
@@ -1,3 +1,19 @@
 from typing import Tuple
 
+import numpy as np
+
 RGBColor = Tuple[int, int, int]
+
+
+def safe_divide(a, b):
+    return np.divide(a, np.maximum(b, .0001))
+
+
+def format_name(x: str) -> str:
+    return ' '.join(i.capitalize() for i in x.split('_'))
+
+
+def hex_to_rgb(hexcode: str) -> Tuple[int, int, int]:
+    hexcode = hexcode[1:]
+
+    return (int(hexcode[:2], 16), int(hexcode[2:4], 16), int(hexcode[4:], 16))
