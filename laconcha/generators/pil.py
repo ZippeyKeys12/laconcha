8d4ae50a906fb3b6d80d405a8beac0abeee5e865
Zain Aamer <ZippeyKeys12@gmail.com>
2020-08-11 23:56:30 -0500

    Maurer rose update

--- laconcha/generators/pil.py
+++ laconcha/generators/pil.py
@@ -23,28 +23,32 @@ def maurer_rose(n: int, d: int, radius: int, color1: RGBColor, color2: RGBColor)
         img = pil_new('RGB', (width, height))
         cx, cy = width // 2, height // 2
 
-        x = cx
-        y = cy
-        coords = []
-
         draw = ImageDraw.Draw(img)
-        for theta in range(361):
-            coords.append((x, y))
 
+        # Webbing
+        coords = [(cx, cy)]
+        for theta in range(1, 360):
             k = radians(theta * d)
             r = radius * sin(n * k)
             x = cx + int(-r * cos(k))
             y = cy + int(-r * sin(k))
+
+            coords.append((x, y))
+        coords.append((cx, cy))
+
         draw.line(coords, fill=color1, width=2)
 
-        coords.clear()
-        for theta in range(361):
-            coords.append((x, y))
-
+        # Outline
+        coords = [(cx, cy)]
+        for theta in range(1, 360):
             k = radians(theta)
             r = radius * sin(n * k)
             x = cx + int(r * cos(k))
             y = cy + int(-r * sin(k))
+
+            coords.append((x, y))
+        coords.append((cx, cy))
+
         draw.line(coords, fill=color2, width=5)
 
         return Image.from_pil(img)
